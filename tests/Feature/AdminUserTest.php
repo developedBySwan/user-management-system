21f37e3f87df833fa1f6a200271e280f43b1cab8
Swan Yee Tun <swanyeetun.2000@gmail.com>
2024-03-04 20:15:38 +0630

    admin user CRUD Add

--- tests/Feature/AdminUserTest.php
+++ tests/Feature/AdminUserTest.php
@@ -27,11 +27,63 @@ function authUser()
     Sanctum::actingAs($authUser);
 }
 
-test('admin user list', function () {
+function adminUserCreate(): AdminUser
+{
+    $rawAdminUser = AdminUser::factory()->raw();
+
+    $adminUser = [
+        ...$rawAdminUser,
+        'role_id' => Role::first()->id,
+    ];
+
+    return AdminUser::create($adminUser);
+}
+
+test('admin user list success', function () {
     authUser();
     $this->getJson("api/v1/admin-user")
         ->assertStatus(200)
         ->assertJsonStructure([
             'data'
         ]);
+});
+
+
+test('admin user detail success', function () {
+    authUser();
+
+    $adminUser = adminUserCreate();
+
+    $this->getJson("api/v1/admin-user/detail/".$adminUser->id)
+        ->assertStatus(200);
+});
+
+test("Admin User Update Success", function () {
+    authUser();
+
+    $adminUser = adminUserCreate();
+
+    $rawAdminUser = AdminUser::factory()->raw();
+
+    $updateAdminUser = [
+        ...$rawAdminUser,
+        'role_id' => Role::first()->id,
+    ];
+
+    $this->putJson("api/v1/admin-user/update/" . $adminUser->id, $updateAdminUser)
+    ->assertStatus(200);
+
+    $this->assertDatabaseHas('admin_users', $updateAdminUser);
+});
+
+test('role delete successfully', function () {
+    authUser();
+
+    $adminUser = adminUserCreate();
+
+    $this->deleteJson("api/v1/admin-user/delete/" . $adminUser->id);
+
+    $this->assertDatabaseMissing('admin_users', [
+        'id' => $adminUser->id
+    ]);
 });
